46427cb73af46da3079584b82cbb2921f5a42231
crisipix <wongc5@gmail.com>
2017-04-30 22:02:13 -0400

    added other vendor packages

--- Angular2Application-VS/ClientApp/app/components/counter/counter.component.ts
+++ Angular2Application-VS/ClientApp/app/components/counter/counter.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
     template: require('./counter.component.html')
 })
 export class CounterComponent {
-    public currentCount = 33;
+    public currentCount = 12;
 
     public incrementCounter() {
         this.currentCount++;
